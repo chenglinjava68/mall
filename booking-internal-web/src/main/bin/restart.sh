f2a7a7c2638b6af1e6427a06ad71eabe162f3fbb
莫冠棠 <guantang.mo@platenogroup.com>
2016-11-23 14:52:22 +0800

    启动脚本增加jvm参数

--- booking-internal-web/src/main/bin/restart.sh
+++ booking-internal-web/src/main/bin/restart.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-ps -ef | grep 'booking-internal-web-0.0.1.jar' | grep -v grep |awk '{print $2}'|xargs kill -9 
+ps -ef | grep 'booking-internal-web-0.0.1.jar' | grep -v grep |awk '{print $2}'|xargs kill 
 
 sleep 5
+
+ps -ef | grep 'booking-internal-web-0.0.1.jar' | grep -v grep |awk '{print $2}'|xargs kill -9
+
+sleep 3
 
 log_date=`date +%Y-%m-%d`
 stdout_log="/data/logs/plateno_mall_booking/plateno_mall_booking_web_stdout.$log_date.log"
 
-nohup java -XX:+HeapDumpOnOutOfMemoryError -jar ./lib/booking-internal-web-0.0.1.jar > $stdout_log 2>&1 &
+nohup java  -XX:+HeapDumpOnOutOfMemoryError -server -d64 -XX:PermSize=200m -XX:MaxPermSize=200m -Xmx1024m -Xms1024m -Xss1m -Xmn350m -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseParallelOldGC -XX:MaxGCPauseMillis=200 -jar ./lib/booking-internal-web-0.0.1.jar > $stdout_log 2>&1 &
