bc84df40ec9dd8738d0e681f4fd11a347cabcee9
莫冠棠 <guantang.mo@platenogroup.com>
2017-02-09 09:50:46 +0800

    供应商后台收货地址显示错误修复（订单详情newAddress返回null）

--- booking-internal-service/src/main/java/com/plateno/booking/internal/service/order/MOrderService.java
+++ booking-internal-service/src/main/java/com/plateno/booking/internal/service/order/MOrderService.java
@@ -1654,7 +1654,12 @@ public class MOrderService{
 				consigneeInfo.setConsigneeName(logc.getConsigneeNewName());
 				consigneeInfo.setMobile(logc.getConsigneeNewMobile());
 			}
-			consigneeInfo.setNewAddress(logc.getNewProvince() + logc.getNewCity() + logc.getNewArea() + logc.getConsigneeNewaddress());
+			
+			String newAddress = StringUtils.trimToEmpty(logc.getNewProvince()) + StringUtils.trimToEmpty(logc.getNewCity()) + StringUtils.trimToEmpty(logc.getNewArea()) + StringUtils.trimToEmpty(logc.getConsigneeNewaddress());
+			if(StringUtils.isBlank(newAddress)) {
+				newAddress = null;
+			}
+			consigneeInfo.setNewAddress(newAddress);
 			consigneeInfo.setNewName(logc.getConsigneeNewName());
 			consigneeInfo.setNewMobile(logc.getConsigneeNewMobile());
 			deliverDetail.setDeliverNo(logc.getLogisticsNo());
