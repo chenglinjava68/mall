3972aa27bb820b2b1544eac5c60ccfb6d114692c
郑楚彬 <tiback.zheng@pp.plateno.cc>
2017-03-28 12:16:29 +0800

    增加druid的web端查看

--- booking-internal-web/src/main/java/com/plateno/booking/internal/JettyServer.java
+++ booking-internal-web/src/main/java/com/plateno/booking/internal/JettyServer.java
@@ -19,39 +19,49 @@ import com.plateno.booking.internal.filter.LogMDCFilter;
 /**
  * @author user
  * 
- *  jetty 启动
+ *         jetty 启动
  *
  */
 public class JettyServer {
-	
-	private static final Logger logger = Logger.getLogger(JettyServer.class);
-	public static void main(String[] args) throws Exception{
-		try {
-        	logger.info("start");
-			InetSocketAddress address = new InetSocketAddress(Config.LOCAL_LISTEN_IP,9577);//post:8083
-			Server server = new Server(address);
-			ServletContextHandler context  = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-			context.setContextPath("/booking-internal-web");
-			DispatcherServlet servlet = new DispatcherServlet();
-			servlet.setContextConfigLocation("classpath:applicationContext.xml");
-			context.addServlet(new ServletHolder(servlet),"/");
-			
-			LogMDCFilter logMDCFilter = new LogMDCFilter();
-			FilterHolder holder = new FilterHolder(logMDCFilter);
-			context.addFilter(holder , "/*", null);
-			
-			server.setHandler(context);
-			server.start();
-			
-            //配置加载
-			WebApplicationContext springContext = servlet.getWebApplicationContext();
-			ConfigLoader loader = springContext.getBean(ConfigLoader.class);
-			loader.init();
 
-			server.join();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private static final Logger logger = Logger.getLogger(JettyServer.class);
+
+    public static void main(String[] args) throws Exception {
+        try {
+            logger.info("start");
+            InetSocketAddress address = new InetSocketAddress(Config.LOCAL_LISTEN_IP, 9577);// post:8083
+            Server server = new Server(address);
+            ServletContextHandler context =
+                    new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+            context.setContextPath("/booking-internal-web");
+            DispatcherServlet servlet = new DispatcherServlet();
+            servlet.setContextConfigLocation("classpath:applicationContext.xml");
+            context.addServlet(new ServletHolder(servlet), "/");
+
+            LogMDCFilter logMDCFilter = new LogMDCFilter();
+            FilterHolder holder = new FilterHolder(logMDCFilter);
+            context.addFilter(holder, "/*", null);
+
+            com.alibaba.druid.support.http.StatViewServlet druidServlet =
+                    new com.alibaba.druid.support.http.StatViewServlet();
+            ServletHolder servletHolder = new ServletHolder(druidServlet);
+            servletHolder.setInitParameter("resetEnable", "true");
+            // servletHolder.setInitParameter("loginUsername", "druid");
+            // servletHolder.setInitParameter("loginPassword", "da453e261fe94650");
+            context.addServlet(servletHolder, "/druid/*");
+            server.setHandler(context);
+
+            server.start();
+
+            // 配置加载
+            WebApplicationContext springContext = servlet.getWebApplicationContext();
+            ConfigLoader loader = springContext.getBean(ConfigLoader.class);
+            loader.init();
+
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
